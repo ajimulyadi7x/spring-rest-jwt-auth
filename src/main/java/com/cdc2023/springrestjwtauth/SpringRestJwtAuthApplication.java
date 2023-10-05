43acd7c050ec5778957a6c9031c4f4a1578b6a6a
Aji Mulyadi <ajiatin.mulyadi@mitrais.com>
2023-10-05 13:57:29 +0700

    Initial commit

---
+++ src/main/java/com/cdc2023/springrestjwtauth/SpringRestJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.cdc2023.springrestjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestJwtAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdc2023/springrestjwtauth/SpringRestJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdc2023.springrestjwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
